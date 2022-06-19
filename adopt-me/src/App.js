aa0f52f09eda268a10e4a94b0bce0c86650cd466
lingardium <karatascanahmet@yahoo.com>
2022-06-19 11:41:05 +0000

    no frills react

---
+++ adopt-me/src/App.js
@@ -0,0 +1,32 @@
+const Pet = (props) => {
+    return React.createElement("div",{},[
+        React.createElement("h1",{},props.name),
+        React.createElement("h2",{},props.animal),
+        React.createElement("h2",{},props.breed)
+    ])
+}
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1",{},"Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Coban",
+            animal: "Dog",
+            breed: "Kangal"
+        }),
+        React.createElement(Pet,{
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel"
+        }),
+        React.createElement(Pet,{
+            name: "Incir",
+            animal: "Cat",
+            breed: "British ShortHair"
+        })
+    )
+ }
+
+ ReactDOM.render(React.createElement(App), document.getElementById("root"));
